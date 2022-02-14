012b92aaaf9da02d8cd4afa4f388e9fc84eb6fdf
Kamil <99438388+kamilsft@users.noreply.github.com>
2022-02-13 23:39:28 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.main-slider', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidePerview: 2,
+    loop: true,
+    navigation: {
+        nextEl: '.main-slider-arrow',
+    }
+});
